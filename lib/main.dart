ad574ad7ebc9d3eee9ec511a6d0dde18d59be3dc
finma <firmanmaulana2403@gmail.com>
2023-07-25 22:14:01 +0700

    [CFG] theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import '/config/app_color.dart';
 import '/routes/router.dart';
 
 void main() {
@@ -13,6 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.interTextTheme(),
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: AppColor.primary,
+        colorScheme: const ColorScheme.light(
+          primary: AppColor.primary,
+          secondary: AppColor.secondary,
+        ),
+      ),
       routerConfig: router,
     );
   }
