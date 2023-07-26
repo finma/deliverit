75b5019fe64fc31d466b7c192eea716f8b180dcb
finma <firmanmaulana2403@gmail.com>
2023-07-26 15:43:22 +0700

    [ADD] color for gradient

--- lib/config/app_color.dart
+++ lib/config/app_color.dart
@@ -3,4 +3,6 @@ import 'package:flutter/animation.dart';
 class AppColor {
   static const primary = Color(0xff5669FF);
   static const secondary = Color(0xffD2DEEE);
+
+  static const gradient = Color(0xffF8D1D1);
 }
