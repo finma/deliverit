c468e96777a31c1af0932a5b5edd1b897f53186a
finma <firmanmaulana2403@gmail.com>
2023-07-27 09:42:01 +0700

    [ADD] countdown cubit

---
+++ lib/cubit/countdown_cubit.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CountdownCubit extends Cubit<int> {
+  final int initialCountdown;
+  late Timer _timer;
+
+  CountdownCubit(this.initialCountdown) : super(initialCountdown);
+
+  void startCountdown() {
+    emit(initialCountdown);
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (state > 0) {
+        emit(state - 1);
+      } else {
+        _timer.cancel();
+      }
+    });
+  }
+
+  void dispose() {
+    _timer.cancel();
+  }
+
+  @override
+  Future<void> close() {
+    _timer.cancel();
+    return super.close();
+  }
+}
