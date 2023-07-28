3b78055afb8f493c4c26b34da2ca837544951db7
finma <firmanmaulana2403@gmail.com>
2023-07-28 11:00:36 +0700

    [ADD] telphone page

---
+++ lib/pages/telphone_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TelphonePage extends StatelessWidget {
+  const TelphonePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Telphone Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
