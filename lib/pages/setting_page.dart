3a972d0f660afa1ab4c53e3f31b573fd7dbf169b
finma <firmanmaulana2403@gmail.com>
2023-07-28 21:01:25 +0700

    [CHG] styling setting page

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,3 +1,4 @@
+import 'package:deliverit/routes/router.dart';
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
@@ -13,6 +14,75 @@ class SettingPage extends StatelessWidget {
             fontSize: 16,
             fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 28),
+        child: Column(
+          children: const [
+            ItemSetting(
+              title: 'Alamat saya',
+              route: Routes.myAddress,
+            ),
+            SizedBox(height: 22),
+            ItemSetting(
+              title: 'Metode pembayaran',
+              route: Routes.paymentMethod,
+            ),
+            SizedBox(height: 22),
+            ItemSetting(
+              title: 'Ubah kata sandi',
+              route: Routes.changePassword,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemSetting extends StatelessWidget {
+  final String title;
+  final String route;
+
+  const ItemSetting({
+    Key? key,
+    required this.title,
+    required this.route,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // debugPrint('$title');
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 2,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            const Icon(Icons.arrow_forward_ios_rounded),
+          ],
         ),
       ),
     );
