3071f6914ba3c867326aa738d3c9ca7d7660af6e
finma <firmanmaulana2403@gmail.com>
2023-07-28 11:00:18 +0700

    [ADD] mitra page

---
+++ lib/pages/mitra_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MitraPage extends StatelessWidget {
+  const MitraPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Mitra Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
