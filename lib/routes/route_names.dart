b95b188bb8b0349261935e1f01cb2b33e116770c
finma <firmanmaulana2403@gmail.com>
2023-07-28 15:22:59 +0700

    [ADD] route names

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -1,10 +1,16 @@
 part of 'router.dart';
 
 class Routes {
+  static const String account = 'account';
+  static const String help = 'help';
+  static const String history = 'history';
   static const String home = 'home';
   static const String login = 'login';
+  static const String message = 'message';
+  static const String mitraFavourite = 'mitra-favourite';
   static const String otp = 'otp';
   static const String profile = 'profile';
   static const String register = 'register';
   static const String setPassword = 'set-password';
+  static const String setting = 'setting';
 }
