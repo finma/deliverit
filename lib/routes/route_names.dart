8b1c077da9cffa042dbe5990b4b6c69812420568
finma <firmanmaulana2403@gmail.com>
2023-07-28 15:11:38 +0700

    [ADD] profile route name

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -4,6 +4,7 @@ class Routes {
   static const String home = 'home';
   static const String login = 'login';
   static const String otp = 'otp';
+  static const String profile = 'profile';
   static const String register = 'register';
   static const String setPassword = 'set-password';
 }
