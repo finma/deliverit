347ed3a1dca1944e70c6ef00528ff5c2742dd815
finma <firmanmaulana2403@gmail.com>
2023-07-28 20:34:08 +0700

    [ADD] setting page

---
+++ lib/pages/setting_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Pengaturan',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
