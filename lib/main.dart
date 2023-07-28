12e29f5216328694dbf08a0d5de065eed0041275
finma <firmanmaulana2403@gmail.com>
2023-07-28 11:01:21 +0700

    [ADD] multiprovider and navigation cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '/cubit/navigation_cubit.dart';
 import '/config/app_color.dart';
 import '/routes/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NavigationCubit()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
