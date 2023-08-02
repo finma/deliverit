3108609bf625ec34d4c013dff80d6e1e78ec1cbc
finma <firmanmaulana2403@gmail.com>
2023-08-02 10:07:45 +0700

    [CHG] update initial value

--- lib/cubit/select_cubit.dart
+++ lib/cubit/select_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SelectCubit extends Cubit<dynamic> {
-  SelectCubit(dynamic initialValue) : super(null);
+  SelectCubit(dynamic initialValue) : super(initialValue);
 
   void setSelectedValue(dynamic value) {
     emit(value);
