ebe2140f332e791fa9389d3828fd548b116971c7
finma <firmanmaulana2403@gmail.com>
2023-08-02 10:05:45 +0700

    [ADD] address data

---
+++ lib/data/address.dart
@@ -0,0 +1,14 @@
+import 'package:deliverit/model/address.dart';
+
+class DataAddress {
+  static Address address1 = Address(
+    name: 'Rumah',
+    address: 'Jl. Pesantren Desa Salagedang',
+    subdistrict: 'Sukahaji',
+    city: 'Majalengka',
+    province: 'Jawa Barat',
+    postalCode: '45471',
+    phoneNumber: '081234567890',
+    isMain: 3,
+  );
+}
