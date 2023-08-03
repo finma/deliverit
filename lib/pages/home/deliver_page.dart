cf07f9804a9c54f823fa4fd535c7e2c4dd4bec0f
finma <firmanmaulana2403@gmail.com>
2023-08-03 09:28:33 +0700

    [ADD] search bar

--- lib/pages/home/deliver_page.dart
+++ lib/pages/home/deliver_page.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '/config/app_asset.dart';
+import '/config/app_color.dart';
+
 class DeliverPage extends StatefulWidget {
   const DeliverPage({super.key});
 
@@ -16,15 +19,17 @@ class _DeliverPageState extends State<DeliverPage> {
   late GoogleMapController newGoogleMapController;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(-7.319563, 108.202972),
     zoom: 14.4746,
   );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: [
+            // * GOOGLE MAP
             GoogleMap(
               mapType: MapType.normal,
               myLocationButtonEnabled: true,
@@ -33,6 +38,79 @@ class _DeliverPageState extends State<DeliverPage> {
                 _controllerGoogleMap.complete(controller);
                 newGoogleMapController = controller;
               },
+            ),
+
+            // * SEARCH BAR
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(24),
+                decoration: const BoxDecoration(
+                  color: AppColor.primary,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Mau kirim barang kemana?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              debugPrint('Cari lokasi tujuan');
+                            },
+                            child: Container(
+                              // width: double.infinity,
+                              height: 32,
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 22),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: Row(
+                                children: const [
+                                  ImageIcon(AssetImage(AppAsset.iconLocation)),
+                                  SizedBox(width: 12),
+                                  Text(
+                                    'Cari lokasi tujuan',
+                                    style: TextStyle(fontSize: 12),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 22),
+                        Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          child: const ImageIcon(AssetImage(AppAsset.iconGps)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
