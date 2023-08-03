50f9b0b967ce07ea910b4a8d86741a5642fd38d6
finma <firmanmaulana2403@gmail.com>
2023-08-03 10:45:54 +0700

    [ADD] get location

--- lib/pages/home/deliver_page.dart
+++ lib/pages/home/deliver_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '/config/app_asset.dart';
@@ -14,9 +15,57 @@ class DeliverPage extends StatefulWidget {
 }
 
 class _DeliverPageState extends State<DeliverPage> {
+  // * CONTROLLER GOOGLE MAP
   final Completer<GoogleMapController> _controllerGoogleMap =
       Completer<GoogleMapController>();
   late GoogleMapController newGoogleMapController;
+
+  // * CURRENT LOCATION
+  Position? currentLocation;
+  var geoLocator = Geolocator();
+  double bottomPaddingOfMap = 0;
+
+  // * FUNCTION TO GET CURRENT LOCATION
+  void locatePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // * CHECK LOCATION SERVICE
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    // * CHECK LOCATION PERMISSION
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // * GET CURRENT LOCATION
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    currentLocation = position;
+
+    // debugPrint('location: $position');
+
+    // * MOVE CAMERA TO CURRENT LOCATION
+    LatLng latlngPosition = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition =
+        CameraPosition(target: latlngPosition, zoom: 14);
+    newGoogleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(-7.319563, 108.202972),
@@ -31,12 +80,23 @@ class _DeliverPageState extends State<DeliverPage> {
           children: [
             // * GOOGLE MAP
             GoogleMap(
+              padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
               mapType: MapType.normal,
               myLocationButtonEnabled: true,
               initialCameraPosition: _kGooglePlex,
+              myLocationEnabled: true,
+              zoomControlsEnabled: true,
+              zoomGesturesEnabled: true,
               onMapCreated: (controller) {
                 _controllerGoogleMap.complete(controller);
                 newGoogleMapController = controller;
+
+                setState(() {
+                  bottomPaddingOfMap = 120;
+                });
+
+                // * GET CURRENT LOCATION
+                locatePosition();
               },
             ),
 
