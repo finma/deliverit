1ec4f0a3286caae22e9fdfe6224024e629c6d1ad
finma <firmanmaulana2403@gmail.com>
2023-08-05 22:14:13 +0700

    [CHG] add is obtain direction

--- lib/cubit/deliver/deliver_state.dart
+++ lib/cubit/deliver/deliver_state.dart
@@ -3,8 +3,10 @@ part of 'deliver_cubit.dart';
 class DeliverState {
   final MapAddress? pickUpAddress;
   final MapAddress? dropOffAddress;
+  final bool? isObtainDirection;
 
-  DeliverState({this.pickUpAddress, this.dropOffAddress});
+  DeliverState({this.pickUpAddress, this.dropOffAddress})
+      : isObtainDirection = (pickUpAddress != null && dropOffAddress != null);
 
   DeliverState copyWith({
     MapAddress? pickUpAddress,
