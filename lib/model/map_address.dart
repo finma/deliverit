e98b4da605494522834b2514382b42123b16170d
finma <firmanmaulana2403@gmail.com>
2023-08-05 11:21:05 +0700

    [ADD] model map address

---
+++ lib/model/map_address.dart
@@ -0,0 +1,31 @@
+class MapAddress {
+  String? placeFormattedAddress;
+  String? placeName;
+  String? placeId;
+  String? latitude;
+  String? longitude;
+
+  MapAddress({
+    this.placeFormattedAddress,
+    this.placeName,
+    this.placeId,
+    this.latitude,
+    this.longitude,
+  });
+
+  factory MapAddress.fromJson(Map<String, dynamic> json) => MapAddress(
+        placeFormattedAddress: json["place_formatted_address"],
+        placeName: json["place_name"],
+        placeId: json["place_id"],
+        latitude: json["latitude"],
+        longitude: json["longitude"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "place_formatted_address": placeFormattedAddress,
+        "place_name": placeName,
+        "place_id": placeId,
+        "latitude": latitude,
+        "longitude": longitude,
+      };
+}
