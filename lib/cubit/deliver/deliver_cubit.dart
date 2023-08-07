0ba266876cb87f9fc0872d8c82c6a8871e726da7
finma <firmanmaulana2403@gmail.com>
2023-08-07 10:04:30 +0700

    [CHG] add polyline state

--- lib/cubit/deliver/deliver_cubit.dart
+++ lib/cubit/deliver/deliver_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '/model/map_address.dart';
 
@@ -6,7 +7,14 @@ part 'deliver_state.dart';
 
 class DeliverCubit extends Cubit<DeliverState> {
   DeliverCubit()
-      : super(DeliverState(pickUpAddress: null, dropOffAddress: null));
+      : super(DeliverState(
+          pickUpAddress: null,
+          dropOffAddress: null,
+          pLineCoordinates: [],
+          polylineSet: {},
+          markerSet: {},
+          circleSet: {},
+        ));
 
   void setPickUpAddress(MapAddress address) {
     emit(state.copyWith(pickUpAddress: address));
@@ -14,5 +22,61 @@ class DeliverCubit extends Cubit<DeliverState> {
 
   void setDropOffAddress(MapAddress address) {
     emit(state.copyWith(dropOffAddress: address));
+  }
+
+  void setIsLocationUpdated(bool value) {
+    emit(state.copyWith(isLocationUpdated: value));
+  }
+
+  // create void to add new pline coordinates to array pline coordinates
+  void addPLineCoordinates(LatLng coordinate) {
+    final List<LatLng> newPlineCoordinates = [...state.pLineCoordinates];
+    newPlineCoordinates.add(coordinate);
+    emit(state.copyWith(pLineCoordinates: newPlineCoordinates));
+  }
+
+  // create void to add new poliline to set poliline
+  void addPolyline(Polyline polyline) {
+    final Set<Polyline> newPolylineSet = {...state.polylineSet};
+    newPolylineSet.add(polyline);
+    emit(state.copyWith(polylineSet: newPolylineSet));
+  }
+
+  // create void to add new marker to set marker
+  void addMarker(Marker marker) {
+    final Set<Marker> newMarkerSet = {...state.markerSet};
+    newMarkerSet.add(marker);
+    emit(state.copyWith(markerSet: newMarkerSet));
+  }
+
+  // create void to add new circle to set circle
+  void addCircle(Circle circle) {
+    final Set<Circle> newCircleSet = {...state.circleSet};
+    newCircleSet.add(circle);
+    emit(state.copyWith(circleSet: newCircleSet));
+  }
+
+  void clearPickUpAddress() {
+    emit(state.copyWith(pickUpAddress: null));
+  }
+
+  void clearDropOffAddress() {
+    emit(state.copyWith(dropOffAddress: null));
+  }
+
+  void clearPLinesCoordinates() {
+    emit(state.copyWith(pLineCoordinates: []));
+  }
+
+  void clearPolylineSet() {
+    emit(state.copyWith(polylineSet: {}));
+  }
+
+  void clearMarkerSet() {
+    emit(state.copyWith(markerSet: {}));
+  }
+
+  void clearCircleSet() {
+    emit(state.copyWith(circleSet: {}));
   }
 }
--- lib/cubit/deliver/deliver_state.dart
+++ lib/cubit/deliver/deliver_state.dart
@@ -4,17 +4,57 @@ class DeliverState {
   final MapAddress? pickUpAddress;
   final MapAddress? dropOffAddress;
   final bool? isObtainDirection;
+  final bool isLocationUpdated;
+  final List<LatLng> pLineCoordinates;
+  final Set<Polyline> polylineSet;
+  final Set<Marker> markerSet;
+  final Set<Circle> circleSet;
 
-  DeliverState({this.pickUpAddress, this.dropOffAddress})
-      : isObtainDirection = (pickUpAddress != null && dropOffAddress != null);
+  DeliverState({
+    this.pickUpAddress,
+    this.dropOffAddress,
+    bool? isLocationUpdated,
+    List<LatLng>? pLineCoordinates,
+    Set<Polyline>? polylineSet,
+    Set<Marker>? markerSet,
+    Set<Circle>? circleSet,
+  })  : pLineCoordinates = pLineCoordinates ?? [],
+        polylineSet = polylineSet ?? {},
+        markerSet = markerSet ?? {},
+        circleSet = circleSet ?? {},
+        isLocationUpdated = isLocationUpdated ?? false,
+        isObtainDirection = (pickUpAddress != null && dropOffAddress != null);
 
   DeliverState copyWith({
     MapAddress? pickUpAddress,
     MapAddress? dropOffAddress,
+    bool? isLocationUpdated,
+    List<LatLng>? pLineCoordinates,
+    Set<Polyline>? polylineSet,
+    Set<Marker>? markerSet,
+    Set<Circle>? circleSet,
   }) {
     return DeliverState(
       pickUpAddress: pickUpAddress ?? this.pickUpAddress,
       dropOffAddress: dropOffAddress ?? this.dropOffAddress,
+      isLocationUpdated: isLocationUpdated ?? this.isLocationUpdated,
+      pLineCoordinates: pLineCoordinates ?? this.pLineCoordinates,
+      polylineSet: polylineSet ?? this.polylineSet,
+      markerSet: markerSet ?? this.markerSet,
+      circleSet: circleSet ?? this.circleSet,
     );
+  }
+
+  // create to json state
+  Map<String, dynamic> toJson() {
+    return {
+      'pickUpAddress': pickUpAddress?.toJson(),
+      'dropOffAddress': dropOffAddress?.toJson(),
+      'isLocationUpdated': isLocationUpdated,
+      'pLineCoordinates': pLineCoordinates.map((e) => e.toJson()).toList(),
+      'polylineSet': polylineSet.map((e) => e.toJson()).toList(),
+      'markerSet': markerSet.map((e) => e.toJson()).toList(),
+      'circleSet': circleSet.map((e) => e.toJson()).toList(),
+    };
   }
 }
