57356543ee649a2bd607d92f18e46ca525ccd09f
finma <firmanmaulana2403@gmail.com>
2023-08-10 21:57:19 +0700

    [CHG] update data payload

--- lib/data/payload.dart
+++ lib/data/payload.dart
@@ -2,10 +2,17 @@ import 'package:deliverit/model/payload.dart';
 
 class DataPayload {
   static final all = <Payload>[
+    Payload(name: 'Kardus', size: PayloadSize.small),
+    Payload(name: 'Kardus', size: PayloadSize.medium),
     Payload(name: 'Kardus', size: PayloadSize.large),
+    Payload(name: 'Kasur', size: PayloadSize.medium),
     Payload(name: 'Kasur', size: PayloadSize.large),
+    Payload(name: 'Meja', size: PayloadSize.medium),
     Payload(name: 'Meja', size: PayloadSize.large),
+    Payload(name: 'Lemari', size: PayloadSize.medium),
     Payload(name: 'Lemari', size: PayloadSize.large),
+    Payload(name: 'Koper', size: PayloadSize.small),
+    Payload(name: 'Koper', size: PayloadSize.medium),
     Payload(name: 'Koper', size: PayloadSize.large),
   ];
 }
