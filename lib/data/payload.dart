2db26474169a614a1ab4e4108b7607313658baf2
finma <firmanmaulana2403@gmail.com>
2023-08-10 17:00:19 +0700

    [ADD] data payload

---
+++ lib/data/payload.dart
@@ -0,0 +1,11 @@
+import 'package:deliverit/model/payload.dart';
+
+class DataPayload {
+  static final all = <Payload>[
+    Payload(name: 'Kardus', size: 'besar'),
+    Payload(name: 'Kasur', size: 'besar'),
+    Payload(name: 'Meja', size: 'besar'),
+    Payload(name: 'Lemari', size: 'besar'),
+    Payload(name: 'Koper', size: 'besar'),
+  ];
+}
