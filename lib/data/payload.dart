c648c23de215d9cd98a78b404198e4b32a8878e6
finma <firmanmaulana2403@gmail.com>
2023-08-12 11:08:48 +0700

    [CHG] update qty payload data

--- lib/data/payload.dart
+++ lib/data/payload.dart
@@ -2,17 +2,17 @@ import 'package:deliverit/model/payload.dart';
 
 class DataPayload {
   static final all = <Payload>[
-    Payload(name: 'Kardus', size: PayloadSize.small),
-    Payload(name: 'Kardus', size: PayloadSize.medium),
-    Payload(name: 'Kardus', size: PayloadSize.large),
-    Payload(name: 'Kasur', size: PayloadSize.medium),
-    Payload(name: 'Kasur', size: PayloadSize.large),
-    Payload(name: 'Meja', size: PayloadSize.medium),
-    Payload(name: 'Meja', size: PayloadSize.large),
-    Payload(name: 'Lemari', size: PayloadSize.medium),
-    Payload(name: 'Lemari', size: PayloadSize.large),
-    Payload(name: 'Koper', size: PayloadSize.small),
-    Payload(name: 'Koper', size: PayloadSize.medium),
-    Payload(name: 'Koper', size: PayloadSize.large),
+    Payload(name: 'Kardus', size: PayloadSize.small, qty: 1),
+    Payload(name: 'Kardus', size: PayloadSize.medium, qty: 1),
+    Payload(name: 'Kardus', size: PayloadSize.large, qty: 1),
+    Payload(name: 'Kasur', size: PayloadSize.medium, qty: 1),
+    Payload(name: 'Kasur', size: PayloadSize.large, qty: 1),
+    Payload(name: 'Meja', size: PayloadSize.medium, qty: 1),
+    Payload(name: 'Meja', size: PayloadSize.large, qty: 1),
+    Payload(name: 'Lemari', size: PayloadSize.medium, qty: 1),
+    Payload(name: 'Lemari', size: PayloadSize.large, qty: 1),
+    Payload(name: 'Koper', size: PayloadSize.small, qty: 1),
+    Payload(name: 'Koper', size: PayloadSize.medium, qty: 1),
+    Payload(name: 'Koper', size: PayloadSize.large, qty: 1),
   ];
 }
