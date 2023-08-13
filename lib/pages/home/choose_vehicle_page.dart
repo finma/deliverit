cecfd7df34b987194cbda26a9358d2a6976d7f97
finma <firmanmaulana2403@gmail.com>
2023-08-13 11:12:32 +0700

    [ADD] list vehicles

--- lib/pages/home/choose_vehicle_page.dart
+++ lib/pages/home/choose_vehicle_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '/config/app_color.dart';
+import '/config/app_format.dart';
+import '/cubit/select_cubit.dart';
+import '/data/vehicle.dart';
+import '/model/vehicle.dart';
+
 class ChooseVehiclePage extends StatelessWidget {
-  const ChooseVehiclePage({super.key});
+  ChooseVehiclePage({super.key});
+
+  final List<Vehicle> vehicles = DataVehicle.all;
+  final SelectCubit<int> selectVehicle = SelectCubit(0);
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +36,121 @@ class ChooseVehiclePage extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      body: Center(
-        child: Text('Choose Vehicle'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [_buildListVehicles(context)],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container _buildListVehicles(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      height: MediaQuery.of(context).size.height * 0.8,
+      child: ListView.separated(
+        separatorBuilder: (context, index) => const SizedBox(height: 16),
+        itemCount: vehicles.length,
+        itemBuilder: (context, index) {
+          final vehicle = vehicles[index];
+
+          // * LIST ITEM
+          return Material(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(24),
+            child: InkWell(
+              onTap: () => selectVehicle.setSelectedValue(vehicle.id),
+              borderRadius: BorderRadius.circular(24),
+              child: BlocBuilder<SelectCubit<int>, int>(
+                bloc: selectVehicle,
+                builder: (context, value) {
+                  return Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: value == vehicle.id
+                                ? AppColor.primary
+                                : Colors.transparent,
+                            width: 1.5,
+                          ),
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.asset(
+                                  vehicle.image,
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 16),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  vehicle.name,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  'Berat maks: ${vehicle.maxWeight} Kg',
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                                Text(
+                                  'Biaya /km: ${AppFormat.currency(vehicle.price.toDouble())}',
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // * CHIP
+                      if (value == vehicle.id)
+                        const Positioned(
+                          top: 2,
+                          right: 8,
+                          child: Chip(
+                            backgroundColor: AppColor.primary,
+                            elevation: 0,
+                            visualDensity: VisualDensity.compact,
+                            label: Text(
+                              'dipilih',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
