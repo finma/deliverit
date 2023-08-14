20d11d0cdccae465cd43b5aa63e5dd7d73f4aec1
finma <firmanmaulana2403@gmail.com>
2023-08-14 10:01:46 +0700

    [ADD] detail location card

--- lib/pages/home/delivery_detail_page.dart
+++ lib/pages/home/delivery_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '/config/app_color.dart';
+
 class DeliveryDetailPage extends StatelessWidget {
   const DeliveryDetailPage({super.key});
 
@@ -25,6 +27,150 @@ class DeliveryDetailPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_rounded),
           color: Colors.black,
         ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            _buildCardAddress(
+              placeName: 'Cikara Studio',
+              address:
+                  'Perum Cipta Graha Mandiri Blok C 108, Sukarindik, Kec.Bungursari, Tasikmalaya',
+              phoneNumber: '081123456789',
+              userName: 'Firman Maulana',
+              note: '',
+            ),
+            const SizedBox(height: 24),
+            _buildCardAddress(
+              placeName: 'Jl.Kona 1',
+              address:
+                  'Jl.Kona 1 sukabungur , Sukarindik, Kec.Bungursari, Tasikmalaya',
+              phoneNumber: '081123456789',
+              userName: 'Maulana',
+              // note: '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildCardAddress({
+    required String userName,
+    required String placeName,
+    required String address,
+    required String phoneNumber,
+    String? note,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Colors.grey.withOpacity(0.5),
+        ),
+        //create box shadow
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Column(
+                children: [
+                  Icon(
+                    Icons.location_on_outlined,
+                    color: AppColor.primary,
+                  ),
+                ],
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 3),
+                    const Text(
+                      'Alamat Pengambilan',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      placeName,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 6),
+                    Text(
+                      address,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const Divider(thickness: 1, height: 24),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Icon(Icons.person_outline_rounded, color: Colors.grey),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 3),
+                    Text(
+                      '$phoneNumber ($userName)',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Icon(Icons.outlined_flag_rounded, color: Colors.grey),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 3),
+                    Text(
+                      note != null && note != '' ? note : 'Tidak ada catatan',
+                      // note  ?? 'Tidak ada catatan',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
