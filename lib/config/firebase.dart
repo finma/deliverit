20e1277747afc58df8bdc1caf06e1d1ee86b8ecd
finma <firmanmaulana2403@gmail.com>
2023-09-12 14:06:26 +0700

    [ADD] driver ref, server token, request time

--- lib/config/firebase.dart
+++ lib/config/firebase.dart
@@ -1,3 +1,9 @@
 import 'package:firebase_database/firebase_database.dart';
 
 DatabaseReference userRef = FirebaseDatabase.instance.ref().child('users');
+DatabaseReference driverRef = FirebaseDatabase.instance.ref().child('drivers');
+
+String serverToken =
+    'key=AAAAYQwGXaY:APA91bG-sws_GPeMsEkUNx-uthw7lZx7fROrbyxWZgnTa4eiOcws1_yMEKm0UtMhpXLwaUMw9lDGub7U6Tk8TBfrXrQ7LCDgn_fcI7xUjxZheDtpOQKE6qfqCbtMGaxMzEB7SexhgabO';
+
+int driverRequestTimeout = 30;
